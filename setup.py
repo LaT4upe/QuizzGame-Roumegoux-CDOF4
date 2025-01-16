75b16da7def0979606b1d448fee160288bd0f66b
LaT4upe <LaT4upe@gmail.com>
2025-01-16 16:40:46 +0100

    Add Quizz.py and Questions.csv

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='QuizzGame',
+ version='1.0',
+ author='Lat4upe',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
